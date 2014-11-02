06d20cfd72205f4ef1769b048d7ec5de4bf8ea3a
veapon <veapon88@gmail.com>
2014-11-02 21:04:55 +0800

    update

--- rss.js
+++ rss.js
@@ -1,97 +1,114 @@
 // transmission's watch_dir
-var torrent_dir = '/share/torrents/';
+var torrent_dir = 'torrents/';
 
 // RSS url
-var rss_url = 'http://pt.hd4fans.org/torrentrss.php';
+var rss_url = [
+	'http://pt.hd4fans.org/torrentrss.php?rows=10&linktype=dl&passkey=xxx&inclbookmarked=1'
+	,'https://hdcmct.org/torrentrss.php?rows=10&linktype=dl&passkey=xxx&inclbookmarked=1'
+	];
 
 rss();
-log('test');
 
-// retrieve rss feed ever 30 mins
+
+// retrieve rss feed every 30 mins
 setInterval(function(){
-    rss(); 
+	rss(); 
 }, 1800000)
 
 function die(msg) {
-    console.log(msg);
+	console.log(msg);
 }
 
 function rss()
 {
-    var FP = require('feedparser');
-    var request = require('request');
-    var fs = require('fs');
+	var FP = require('feedparser');
+	var request = require('request');
+	var fs = require('fs');
 
-    var req = request(rss_url);
-    var feedparser = new FP({});
+	var req = request(rss_url);
+	var feedparser = new FP({});
 
-    req.on('error', function (error) {
-        die('Error occurred while loading RSS, please check your rss_url.');
-    });
+	req.on('error', function (error) {
+		die('Error occurred while loading RSS, please check your rss_url.');
+	});
 
-    req.on('response', function (res) {
-        var stream = this;
-        if (res.statusCode != 200) {
-           die('Error occurred while loading RSS, please check your rss_url.');
-            return false;
-        }
-        stream.pipe(feedparser);
-    });
+	req.on('response', function (res) {
+		var stream = this;
+		if (res.statusCode != 200) {
+			die('Error occurred while loading RSS, please check your rss_url.');
+			return false;
+		}
+		stream.pipe(feedparser);
+	});
 
-    feedparser.on('readable', function() {
-        var stream = this;
-        var meta = this.meta;
-        var item;
+	feedparser.on('readable', function() {
+		var stream = this;
+		var meta = this.meta;
+		var item;
 
-        while (item = stream.read()) {
-            console.log('['+new Date().toLocaleString()+']Downloading torrent: '+item.enclosures[0].url);
+		while (item = stream.read()) {
+			//console.log('['+new Date().toLocaleString()+']Downloading torrent: '+item.enclosures[0].url);
 
-            // destination file, replace space with dot(.)
-            var dest = torrent_dir + item.title.replace(/\s/g, '.') + '.torrent';
-            
-            // the torrent url
-            var torrent_url = item.enclosures[0].url;
+			// destination file, replace space with dot(.)
+			var dest = torrent_dir + item.title.replace(/\s/g, '.') + '.torrent';
 
-            fs.exists(dest, function (exists) {
+			// the torrent url
+			var torrent_url = item.enclosures[0].url;
 
-                if (exists) {
-                    die('Torrent :'+dest+' exists, skipped.');
-                } else {
-                    require('http').get(torrent_url, function(res){
+			// break if the url is exists
+			var logs = get_log();
+			if (typeof logs != 'undefined' && logs.indexOf(torrent_url) > -1) {
+				continue;
+			}
 
-                        res.on('error', function(err){
-                            die(err);
-                        })
+			require('http').get(torrent_url, function(res){
 
-                        // create and download torrent file
-                        var file = fs.createWriteStream(dest);
-                        res.pipe(file);
+				res.on('error', function(err){
+					die(err);
+				})
 
-                        file.on('finish', function(){
-                            console.log('['+new Date().toLocaleString()+']Download success: '+dest);
-                            file.close();
-                        })
+				// create and download torrent file
+				var file = fs.createWriteStream(dest);
+				res.pipe(file);
 
-                        file.on('error', function(err){
-                            file.close();
-                            die('['+new Date().toLocaleString()+']Download failed: torrent_url, '+err);
-                        })
-                    });
-                    // http.get() end
-                }
-                // exists end
+				file.on('finish', function(){
+					var logs = get_log();
+					console.log(torrent_url);
+					if (typeof logs != 'undefined' && /\S/.test(logs)) {
+						set_log(',' + torrent_url);
+					} else {
+						set_log(torrent_url);
+					}
+					console.log('['+new Date().toLocaleString()+']Download success: '+torrent_url);
+					file.close();
+				})
 
-            });
-        }
-        // stream.read() end
+				file.on('error', function(err){
+					file.close();
+					die('['+new Date().toLocaleString()+']Download failed: torrent_url, '+err);
+				})
+			});
+			// http.get() end
 
-    });
+		}
+		// stream.read() end
+
+	});
 }
 
-function log(str)
+function set_log(str)
 {
-    var fs = require('fs');
-    var log_path = './logs/rss.log';
-    
-    fs.appendFileSync(log_path, str+"\n", {flag: 'a+'});
+	var fs = require('fs');
+	var log_path = './logs/rss.log';
+
+	fs.writeFileSync(log_path, str, {flag: 'a+'});
+	return true;
+}
+
+function get_log()
+{
+	var fs = require('fs');
+	var log_path = './logs/rss.log';
+
+	return fs.readFileSync(log_path, {encoding: 'utf-8', flag: 'a+'});
 }
