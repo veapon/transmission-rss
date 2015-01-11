60b92caed4a7c895d86ad5e359550bc50c365e12
veapon <veapon88@gmail.com>
2015-01-11 21:58:40 +0800

    Fixed a path problem when start from scripts/run.sh

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-// transmission's watch-dir
 global.torrent_dir = '/share/Torrents/';
 
-// rss urls, replace Your_passkey with your real passkey
 global.rss_urls = [
-	'http://pt.hd4fans.org/torrentrss.php?rows=10&linktype=dl&inclbookmarked=1&passkey=Your_passkey'
-	,'https://hdcmct.org/torrentrss.php?rows=10&linktype=dl&inclbookmarked=1&passkey=Your_passkey'
-	,'https://tp.m-team.cc/torrentrss.php?rows=10&linktype=dl&inclbookmarked=1&passkey=Your_passkey'
-	,'http://totheglory.im/putrssmc.php?par=ttg_string'
+	'http://pt.hd4fans.org/torrentrss.php?rows=10&linktype=dl&inclbookmarked=1&passkey=47bc1ed13feef5ea2baf68584f8b3a3a'
+	,'https://hdcmct.org/torrentrss.php?rows=10&linktype=dl&inclbookmarked=1&passkey=d3cb418c4293e7141087c8d44f51b49d'
+	,'https://tp.m-team.cc/torrentrss.php?rows=10&linktype=dl&inclbookmarked=1&passkey=77c8c54c243d114a32b81ff3741b6560'
+	,'http://totheglory.im/putrssmc.php?par=dnZ2MTM4ODAxLGUwM2NiZDU0MjE4NmJhMzViYTg4N2U1NTg1NmQwOWY3eno='
 	];
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 root=$(dirname $(pwd))
+cd $root
 pm2 start $root/rss.js -n transmission-rss -e $root/logs/rss-err.log -o $root/logs/rss.log
 
